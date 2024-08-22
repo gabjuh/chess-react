d09fc119fa5f70746fd2cca747ca4c5e21b376d5
Gábor Juhász <g.juhasz@neusta.de>
2024-08-22 22:28:50 +0200

    add transition

--- src/conponents/Field.tsx
+++ src/conponents/Field.tsx
@@ -23,11 +23,11 @@ const Field: React.FC<FieldType> = ({
   return (
     <div 
       onClick={() => piece && handlePieceClick(coords)}
-      className={`select-none relative w-20 h-20 text-white text-center ${piece ? 'cursor-pointer hover:bg-gray-400' : ''} ${isDark ? 'bg-gray-600' : 'bg-gray-500'} ${isHighlighted ? '!bg-green-800 opacity-60' : ''} ${isSelected ? 'border-[3px] border-green-400' : ''}`}>
+      className={`select-none relative w-20 h-20 text-white text-center box-border transition-all ease-in-out duration-150 ${piece ? 'cursor-pointer hover:bg-gray-400' : ''} ${isDark ? 'bg-gray-600' : 'bg-gray-500'} ${isHighlighted ? '!bg-green-800 opacity-60' : ''} ${isSelected ? 'border-[3px] border-green-400' : ''}`}>
 
       {isShowingChords && <div className="absolute text-[.8rem] right-1 top-1">{`[${coords[0]}/${coords[1]}]`}</div>}
 
-      <span>{piece}</span>
+      <span className={`block transition-all duration-200 ease-in-out ${isSelected ? '' : ''}`}>{piece}</span>
 
     </div>
   )
