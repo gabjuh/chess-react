f74271f6d6797b32767499ded5b68cd9cdffe70f
Gábor Juhász <g.juhasz@neusta.de>
2024-08-22 22:52:51 +0200

    add transitions, isMoveable feat.

--- src/conponents/Field.tsx
+++ src/conponents/Field.tsx
@@ -8,6 +8,7 @@ interface FieldType  {
   handlePieceClick: (coords: number[]) => void;
   isHighlighted: boolean;
   isShowingChords: boolean;
+  isMoveable: boolean;
 }
 
 const Field: React.FC<FieldType> = ({
@@ -17,17 +18,20 @@ const Field: React.FC<FieldType> = ({
   isSelected,
   handlePieceClick,
   isHighlighted,
-  isShowingChords
+  isShowingChords,
+  isMoveable
 }) => {
   
   return (
     <div 
       onClick={() => piece && handlePieceClick(coords)}
-      className={`select-none relative w-20 h-20 text-white text-center box-border transition-all ease-in-out duration-150 ${piece ? 'cursor-pointer hover:bg-gray-400' : ''} ${isDark ? 'bg-gray-600' : 'bg-gray-500'} ${isHighlighted ? '!bg-green-800 opacity-60' : ''} ${isSelected ? 'border-[3px] border-green-400' : ''}`}>
+      className={`select-none relative w-20 h-20 text-white text-center box-border transition-all ease-in-out duration-150 ${piece ? 'cursor-pointer hover:bg-gray-400' : ''} ${isDark ? 'bg-gray-600' : 'bg-gray-500'} ${isHighlighted ? 'before:bg-green-600 before:opacity-30 before:content-[""] before:w-full before:h-full before:block' : ''} `}>
 
       {isShowingChords && <div className="absolute text-[.8rem] right-1 top-1">{`[${coords[0]}/${coords[1]}]`}</div>}
 
-      <span className={`block transition-all duration-200 ease-in-out ${isSelected ? '' : ''}`}>{piece}</span>
+      <div className={`block absolute transition-all duration-150 ease-in-out ${isSelected ? isMoveable ? 'border-[3px] border-green-400' : 'border-[5px] border-orange-400' : ''} w-full h-full`}></div>
+
+      <span className={`block transition-all duration-150 ease-in-out ${isSelected && isMoveable  ? 'translate-y-[-4px] scale-[1.15] rotate-[8deg] drop-shadow-[0_5px_5px_rgba(0,0,0,0.6)]' : ''}`}>{piece}</span>
 
     </div>
   )
